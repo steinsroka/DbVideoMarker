9211d273aee19d4587f62a8956b5efc2a529a113
steinsroka <62500287+steinsroka@users.noreply.github.com>
2020-07-13 21:07:19 +0900

    Update Appdatabase.java

--- app/src/main/java/com/example/dbvideomarker/database/Appdatabase.java
+++ app/src/main/java/com/example/dbvideomarker/database/Appdatabase.java
@@ -1,4 +1,31 @@
 package com.example.dbvideomarker.database;
 
-public class Appdatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.dbvideomarker.database.entitiy.Video;
+
+@Database(entities = {Video.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "video")
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+
+    public abstract VideoDao videoModel();
+
 }
